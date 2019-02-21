e77ebadebc5c00333eb8a4b3fe032641b1cefa65
sa6oznaka <treildr@gmail.com>
2019-02-21 20:52:51 +0200

    Added background and transparancy effect on game.js

--- js/game.js
+++ js/game.js
@@ -11,10 +11,16 @@ canvas.width = 1000;
 canvas.height = 600;
 var context = canvas.getContext("2d");
 
+//==========================//
+var Activated = false;
+
+var Transparancy = 0;
+var Transcparacy_Time = 120;
+
 window.addEventListener("keydown", function (args) {
 
-    if(args.keyCode==32) {
-        console.log("Space pressed");
+    if(args.keyCode == 32 && ! Activated) {
+        Activated = true;
     }
 
 
@@ -29,19 +35,39 @@ window.addEventListener("mousemove", function (args) {
 }, false);
 
 function update() {
-
+    
+    if(Activated && Transparancy<Transcparacy_Time){
+        Transparancy = Transparancy + Transparancy*0.02 + 0.2;
+    }
+    
     setTimeout(update, 10);
 }
 function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.globalAlpha = 1;
 
-    context.fillStyle = "#000FF0";
-    context.fillRect(0,0,1000,700);
+    if(Activated){
+        
+        if(Transparancy < Transcparacy_Time){
+            for(var i=0;i<Transparancy;i++){
+                context.fillStyle = "rgba(255, 255, 255, 0.01)";
+                context.fillRect(0,0,1000,700);
+            }
+        }else{
+            context.fillStyle = "rgb(255, 255, 255)";
+            context.fillRect(0,0,1000,700);
+        }
+            
+    }else{
+        //context.fillStyle = "#00FFF0";
+        //context.fillRect(0,0,1000,700);
 
-    context.fillStyle = "#0FF0FF";
-    context.font = "20px Arial";
-    context.fillText("Hello!", 10, 20);
+        context.fillStyle = "#000";
+        context.font = "20px Arial";
+        context.fillText("Hello world There will be credits here!", 10, 20);
+    }
+
+    
 
     requestAnimationFrame(draw);
 }
