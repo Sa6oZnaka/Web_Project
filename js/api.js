b5a9af6a86a2161a36bd55da19667fc2ecd465ce
Alexandar Karapenev <treildr@gmail.com>
2019-03-21 11:31:20 +0200

    Marquee now works with api.js

--- js/api.js
+++ js/api.js
@@ -1,19 +1,22 @@
+var Eth_price,
+	Btc_price;
 
-	        $.get("https://api.coinmarketcap.com/v2/ticker/", function(data, status){
-		        for(var key in data.data){
-		            var coin = data.data[key];
+$.get("https://api.coinmarketcap.com/v2/ticker/", function(data, status){
+	for(var key in data.data){
+		var coin = data.data[key];
 
-		            if(coin.name == "Bitcoin"){
-		              	var Btc_price = coin.quotes.USD.price;
-		              	Btc_price = Math.floor(Btc_price*100)/100;
-			    		document.getElementById("BTC_USD").innerHTML = Btc_price;
-		            }
-		            if(coin.name == "Ethereum"){
-		  				var Eth_price = coin.quotes.USD.price;
-		  				Eth_price = Math.floor(Eth_price*100)/100;
-			    		document.getElementById("ETH_USD").innerHTML = Eth_price;
-		            }
-		        }
-        	});
+		if(coin.name == "Bitcoin"){
+		    Btc_price = coin.quotes.USD.price;
+		    Btc_price = Math.floor(Btc_price*100)/100;
+		   	document.getElementById("BTC_USD").innerHTML = Btc_price;
+		}
+		if(coin.name == "Ethereum"){
+		  	Eth_price = coin.quotes.USD.price;
+		  	Eth_price = Math.floor(Eth_price*100)/100;
+			document.getElementById("ETH_USD").innerHTML = Eth_price;
+		}
+	}
+});
 	        
+
        
--- js/customMarquee.js
+++ js/customMarquee.js
@@ -1,34 +1,43 @@
 
 var canvas;
 
+let x1,
+	x2;
+const speed = 2;
+
 function setup() {
-  	canvas = createCanvas(innerWidth , 20);
+  	canvas = createCanvas(window.innerWidth, 20);
  	canvas.position(0,0);
+
+ 	console.log(width);
+
+ 	x1 = 0;
+ 	x2 = innerWidth / 2;
 }
-
-var x1 = 0;
-var x2;
-
-console.log("hello?");
-
 
 function windowResized() {
     canvas = createCanvas(innerWidth , 20);
  	canvas.position(0,0);
+
+ 	x1 = 0;
+ 	x2 = innerWidth / 2;
 }
 
 function draw() {
-  background(0);
+
+  	background(0);
 	
 	fill(200);
-	text("BITCOIN 4000$", x1, 14);
-	text("ETHEREUM 150$", x2, 14);
+  	textSize(16);
+	text("Bitcoin: " + Btc_price + "$", x1, 15);
+	fill(200);
+	text("Ethereum: " + Eth_price + "$", x2, 15);
 	
-	x1 += 1.74;
-	if(x1 > width){
-		x1 = 0;
-	}
+	x1 += speed;
+	x2 += speed;
+
+	if(x1 > width) x1 = 0;
 	
-	x2 = x1 - width / 2;
+	if(x2 > width) x2 = 0;
 	
 }
